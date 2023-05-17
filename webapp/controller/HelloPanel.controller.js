158eecd1c58949e3670fed0fe04c40f675ce1368
João Vitor Ribeiro Gonçalves <joaoribe234@gmail.com>
2023-05-17 10:33:44 -0300

    passo 16 - walkthrough

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -14,6 +14,19 @@ sap.ui.define([
 
 			// show message
 			MessageToast.show(sMsg);
+		},
+
+		onOpenDialog : function () {
+			// create dialog lazily
+			if (!this.pDialog) {
+				this.pDialog = this.loadFragment({
+					name: "sap.ui.demo.walkthrough.view.HelloDialog"
+				});
+			}
+
+			this.pDialog.then(function(oDialog) {
+				oDialog.open();
+			});
 		}
 	});
 
